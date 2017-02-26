319ca4ded13f045898243a3fc9283e664b1db8d8
Josh Hawkins <jkhaccounts@gmail.com>
2017-02-26 02:33:00 -0600

    Move UI to react, fix style

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import UI from "./components/ui";
 import Map from "./components/map";
 
 class App extends Component {
   render() {
     return (
-      <Map
-        layers={
-          [
-            {
-              url: "https://waterwatch.usgs.gov/index.php?m=real&w=kml&r=us&regions=all",
-              clickable: true
-            },
-            {
-              url: "http://data-huntsvilleal.opendata.arcgis.com/datasets/e7b090d463fc4cdf8fed2bbc2a410e84_5.kml",
-              clickable: false
-            },
-            {
-              url: "https://www.atmos.illinois.edu/iswecs/Google.Earth/Weather.Bundle/Weather.Bundle.kmz",
-              clickable: false
-            },
-            {
-              url: 'http://inciweb.nwcg.gov/feeds/maps/kml?cm.ttl=600',
-              clickable: false
-            }
-          ]
-        }
-      />
+      <div style={{height: 'calc(100% - 50px)'}}>
+        <UI />
+        <Map
+          layers={
+            [
+              {
+                url: "https://waterwatch.usgs.gov/index.php?m=real&w=kml&r=us&regions=all",
+                clickable: true
+              },
+              {
+                url: "http://data-huntsvilleal.opendata.arcgis.com/datasets/e7b090d463fc4cdf8fed2bbc2a410e84_5.kml",
+                clickable: false
+              },
+              {
+                url: "https://www.atmos.illinois.edu/iswecs/Google.Earth/Weather.Bundle/Weather.Bundle.kmz",
+                clickable: false
+              },
+              {
+                url: "http://inciweb.nwcg.gov/feeds/maps/kml?cm.ttl=600",
+                clickable: false
+              },
+              {
+                url: "http://www.crh.noaa.gov/Image/eax/web/Tornado%20Path.kml",
+                clickable: true
+              }
+            ]
+          }
+        />
+      </div>
     );
   }
 }
